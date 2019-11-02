a987c3c37d34d74855e1df423cfc50e25edbbc31
Jacob <jacob.a.feldstein@vanderbilt.edu>
2019-11-02 15:44:34 -0500

    test

--- self-checkout/App.js
+++ self-checkout/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from './components/HomeScreen';
+import DetailsScreen from './components/DetailsScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
-});
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
