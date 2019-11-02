3193935f9fffaca93188a28e1c97044868821931
Jacob <jacob.a.feldstein@vanderbilt.edu>
2019-11-02 16:13:22 -0500

    created Payment and Scan pages

--- self-checkout/App.js
+++ self-checkout/App.js
@@ -1,30 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import {
+//       createStackNavigator,
+//       createAppContainer
+//     } from 'react-navigation-stack';
+// import HomeScreen from './components/HomeScreen';
+// import DetailsScreen from './components/DetailsScreen';
+//
+//
+// const RootStack = createStackNavigator({
+//   Home:{
+//     screen : HomeScreen,
+//
+//   },
+//   Details: {
+//     screen: DetailsScreen,
+//   },
+
+    // initialRouteName: 'Home',
+    // navigationOptions: {
+    //   headerStyle: {
+    //     backgroundColor: '#f4511e',
+    //   },
+    //   headerTintColor: '#fff',
+    //   headerTitleStyle: {
+    //     fontWeight: 'bold',
+    //   },
+    // },
+
+//});
+
+import React, { Component } from 'react';
+import {  createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import HomeScreen from './components/HomeScreen';
 import DetailsScreen from './components/DetailsScreen';
 
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen,
-  },
-  {
-    initialRouteName: 'Home',
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    },
-  },
-);
 
-export default class App extends React.Component {
-  render() {
-    return <RootStack />;
-  }
-}
+const RootStack = createStackNavigator({
+    Home: {
+        screen: HomeScreen,
+    },
+    Details: {
+        screen: DetailsScreen,
+    },
+});
+
+const App = createAppContainer(RootStack);
+
+export default App;
+
+
+//
+// export default class App extends React.Component {
+//   render() {
+//     return <RootStack />;
+//   }
+// }
